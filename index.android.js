aa9ec1f0418f5f0b36ad273be59d2b0a3629015b
Jeremy Greer <jeremy.greer.atl@gmail.com>
2016-08-16 07:08:53 -0500

    initial commit, working toast module

---
+++ index.android.js
@@ -0,0 +1,69 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+import toast from './toast';
+
+class AwesomeProject extends Component {
+  constructor() {
+    super();
+    this.handlePress = this.handlePress.bind(this);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+        <TouchableOpacity onPress={this.handlePress}>
+          <Text>click me to test</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  handlePress() {
+    console.log('woo!');
+    toast.show('It worked.', toast.SHORT);
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
